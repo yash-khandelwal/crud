ae0154715fbd365d4ca34bdf2d367b6ae9b1cf99
yash-khandelwal <kritagya.0398@gmail.com>
2021-02-16 02:51:48 +0530

    sort skip limit pagination added

--- test/read.test.js
+++ test/read.test.js
@@ -2,17 +2,27 @@ import assert from 'assert';
 import {User} from '../src/user.model.js';
 
 describe('Reading users out of the database', () => {
-    let testUser;
+    let testUser, testUser2, testUser3, testUser4;
 
     beforeEach((done) => {
         testUser = new User({
             name: 'kritagya'
         });
-        testUser
-            .save()
-            .then(() => {
-                done();
-            });
+        testUser2 = new User({
+            name: 'yash'
+        });
+        testUser3 = new User({
+            name: 'karthik'
+        });
+        testUser4 = new User({
+            name: 'diary'
+        });
+        Promise.all([
+            testUser.save(),
+            testUser2.save(),
+            testUser3.save(),
+            testUser4.save(),
+        ]).then(() => done());
     })
 
     it('finds all users with name kritagya', (done) => {
@@ -30,4 +40,19 @@ describe('Reading users out of the database', () => {
                 done();
             });
     })
+
+    it('can skip and limit the result set', (done) => {
+        User
+            .find({})
+            .sort({name: 1})
+            .skip(1)
+            .limit(2)
+            .then((users) => {
+                assert(users.length === 2);
+                assert(users[0].name === testUser3.name);
+                assert(users[1].name === testUser.name);
+                done();
+            })
+    });
+
 })
